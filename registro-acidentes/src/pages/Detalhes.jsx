ea622294946751f765c90d2c7fcbfe8af236df5a
luiz-ribeiro06 <luiz.ribeiro06@aluno.ifce.edu.br>
2026-06-23 22:55:13 -0300

    Gerar PDF. Data: 23-06-26

--- registro-acidentes/src/pages/Detalhes.jsx
+++ registro-acidentes/src/pages/Detalhes.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import API_URL from "../services/api";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 function Detalhes() {
   const { id } = useParams();
@@ -34,6 +36,36 @@ function Detalhes() {
     buscarAcidente();
   }, [id]);
 
+  const gerarPDF = async () => {
+    const elemento = document.getElementById("pdf-content");
+
+    const canvas = await html2canvas(elemento);
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF("p", "mm", "a4");
+
+    const imgWidth = 210;
+    const pageHeight = 297;
+
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    let heightLeft = imgHeight;
+    let position = 0;
+
+    pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+
+    heightLeft -= pageHeight;
+
+    while (heightLeft > 0) {
+      position = heightLeft - imgHeight;
+      pdf.addPage();
+      pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+    }
+
+    pdf.save(`acidente-${id}.pdf`);
+  };
+
   if (loading) {
     return <p className="mensagem">Carregando detalhes...</p>;
   }
@@ -47,7 +79,7 @@ function Detalhes() {
   }
 
   return (
-    <div className="detalhes-container">
+    <div id="pdf-content" className="detalhes-container">
       <h1>Detalhes do Acidente</h1>
 
       <div className="dados">
@@ -87,6 +119,7 @@ function Detalhes() {
         </div>
       </div>
 
+      <button onClick={gerarPDF}>Gerar PDF</button>
       <button onClick={() => navigate("/")}>Voltar</button>
     </div>
   );
